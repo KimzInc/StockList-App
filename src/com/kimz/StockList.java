be31adfa5cbe6539f256e6a733059a058769a42b
Kimutai <kimutai0003@gmail.com>
2021-04-29 13:11:04 +0300

    Basket class added

--- src/com/kimz/StockList.java
+++ src/com/kimz/StockList.java
@@ -1,5 +1,6 @@
 package com.kimz;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,10 +18,10 @@ public class StockList {
             StockItem inStock = list.getOrDefault(item.getName(), item);
             //if there are already stocks on this item, adjust quantity
             if (inStock!=item){
-                item.adjustStock(inStock.getQuantityStock());
+                item.adjustStock(inStock.quantityInStock());
             }
             list.put(item.getName(),item);
-            return item.getQuantityStock();
+            return item.quantityInStock();
         }
         return 0;
     }
@@ -28,10 +29,33 @@ public class StockList {
     public int sellStock(String item, int quantity){
         //assumption is that item is on list - null
         StockItem inStock = list.getOrDefault(item, null);
-        if((inStock!=null) && (inStock.getQuantityStock()>=quantity) &&(quantity>0)){
+        if((inStock!=null) && (inStock.quantityInStock()>=quantity) &&(quantity>0)){
             inStock.adjustStock(-quantity);
             return quantity;
         }
-        return 0;
+        return 0; //nothing in stock, not enough or quantity passed is equal or less than zero
+    }
+
+    public StockItem get(String key){
+        return list.get(key); //return object
+    }
+
+    public Map<String, StockItem> Items(){
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+       String s = "\nStock List\n";
+       double totalCost = 0.0;
+       for (Map.Entry<String, StockItem> item : list.entrySet()){
+           StockItem stockItem = item.getValue();
+           double itemValue = stockItem.getPrice();
+
+           s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+           s = s + itemValue + "\n";
+           totalCost+= itemValue;
+       }
+       return s + "Total stock value " + totalCost;
     }
 }
